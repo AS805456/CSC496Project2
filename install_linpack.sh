28ecb90f31ca1f9f77eef54d249386a24df34031
AS805456 <47639969+AS805456@users.noreply.github.com>
2019-04-22 16:53:38 -0400

    Update install_linpack.sh

--- install_linpack.sh
+++ install_linpack.sh
@@ -2,5 +2,4 @@ set -x
 
 wget http://registrationcenter-download.intel.com/akdlm/irc_nas/2169/l_lpk_p_10.3.4.007.tgz
 tar zxvf l_lpk_p_10.3.4.007.tgz
-cd linpack_10.3.4/benchmarks/linpack 
-./xlinpack_xeon64                                          
+cd linpack_10.3.4/benchmarks/linpack                                         
