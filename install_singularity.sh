48535f3142627c659ebd25278b3c0fb47a58fd74
AS805456 <47639969+AS805456@users.noreply.github.com>
2019-04-22 19:16:40 -0400

    Update install_singularity.sh

--- install_singularity.sh
+++ install_singularity.sh
@@ -1,5 +1,6 @@
 set -x
 
+#Install singularity
 wget https://github.com/sylabs/singularity/releases/download/2.4/singularity-2.4.tar.gz                        
 tar zxvf singularity-2.4.tar.gz                                   
 cd singularity-2.4                                               
@@ -7,3 +8,8 @@ cd singularity-2.4
 ./configure --prefix=/usr/local
 make
 sudo make install
+
+#Attempting to build a container
+singularity create test.img
+sudo singularity build test.img
+singularity shell test.img
