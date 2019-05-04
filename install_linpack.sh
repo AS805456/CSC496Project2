60de97ebb3e52bf9cb07d6c67b1faaef3887d634
Osbornable <48108557+Osbornable@users.noreply.github.com>
2019-05-04 17:25:45 -0400

    Update install_linpack.sh

--- install_linpack.sh
+++ install_linpack.sh
@@ -1,4 +1,4 @@
-set -x
+
 #Download, unzip, and move to directory with runnable programs
 sudo wget http://registrationcenter-download.intel.com/akdlm/irc_nas/2169/l_lpk_p_10.3.4.007.tgz
 sudo tar zxvf l_lpk_p_10.3.4.007.tgz
