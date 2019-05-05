549877169d4aed03e8c484e79a9ff2fd2d3a865a
Osbornable <48108557+Osbornable@users.noreply.github.com>
2019-05-04 22:35:56 -0400

    Update install_linpack.sh

--- linpack/install_linpack.sh
+++ linpack/install_linpack.sh
@@ -6,5 +6,6 @@ sudo git clone -b docker --single-branch https://github.com/AS805456/cluster-tem
 cd cluster-template
 sudo wget http://registrationcenter-download.intel.com/akdlm/irc_nas/2169/l_lpk_p_10.3.4.007.tgz
 sudo tar zxvf l_lpk_p_10.3.4.007.tgz
-sudo docker build -t linpack /linpack
+cd linpack 
+sudo docker build -t linpack .
 
