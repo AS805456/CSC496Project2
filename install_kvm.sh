f7de174dc8ffca3df21b72c6463d9af4317e7c23
Osbornable <48108557+Osbornable@users.noreply.github.com>
2019-05-06 13:59:43 -0400

    Update install_kvm.sh

--- install_kvm.sh
+++ install_kvm.sh
@@ -13,11 +13,11 @@ sudo virt-install --name test \
 --vcpus 16 \
 --os-type linux \
 --network network=default \
---graphics vnc,password=foo \
+--graphics none \
 --console pty,target_type=serial \
 --location=ubuntu-16.04.6-server-amd64.iso \
 --initrd-inject cluster-template/Kickstarter.cfg \
---extra-args='ks=file:/Kickstarter.cfg console=tty0 console=ttyS0,115200n8'
+--extra-args='ks=file:/Kickstarter.cfg console=ttyS0,115200n8 serial'
 #This version of the last line not working: sudo virt-install --name test --connect qemu:///system --ram 1024 --disk path=ubuntu.qcow2,format=qcow2,bus=virtio,size=8 --vcpus 8 --os-type linux --network network=default --graphics none --console pty,target_type=serial --location=ubuntu-16.04.6-server-amd64.iso --extra-args 'console=ttyS0,115200n8 serial'
 #For single line copy paste:
 # sudo virt-install --name test1 --connect qemu:///system --ram 16384 --disk path=ubuntu.qcow2,format=qcow2,bus=virtio,size=8 --vcpus 16 --os-type linux --network network=default --graphics none --console pty,target_type=serial --location=ubuntu-16.04.6-server-amd64.iso --initrd-inject cluster-template/Kickstarter.cfg --extra-args='ks=file:/Kickstarter.cfg console=ttyS0,115200n8'
