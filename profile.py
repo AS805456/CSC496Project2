89c638027cbf890ff37779157abf4bed2fad9617
Osbornable <48108557+Osbornable@users.noreply.github.com>
2019-05-09 21:08:25 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -38,7 +38,7 @@ iface.component_id = "eth1"
 iface.addAddress(pg.IPv4Address(prefixForIP + str(1), "255.255.255.0"))
 link.addInterface(iface)
   
-#node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_singularity.sh"))
+node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_singularity.sh"))
   
   
 # Print the RSpec to the enclosing page.
